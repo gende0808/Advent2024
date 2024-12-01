a6756c0ae894ecd6259877c3fc0f42e1e081a3a1
gende0808 <gregiscool@live.nl>
2024-12-01 06:43:14 +0100

    both done yay

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,19 @@
 import * as fs from 'fs';
+
 var os = require('os');
 const input = fs.readFileSync('../Inputs/Day1.txt', 'utf-8');
 
 //Run solution
-one_a(input);
+one_b(input);
 
 
-function one_a(input : string): void {
+function one_a(input: string): void {
     let list_one: number[] = [];
     let list_two: number[] = [];
     let totalSum: number = 0;
     const lines = splitByLine(input);
     lines.forEach(function (line: string): void {
-        list_one.push(Number(line.slice(0,5)));
+        list_one.push(Number(line.slice(0, 5)));
         list_two.push(Number(line.slice(-5)));
     });
     //sort arrays
@@ -24,8 +25,25 @@ function one_a(input : string): void {
     console.log(totalSum);
 }
 
-function one_b(): void {
-
+function one_b(input: string): void {
+    let list_one: number[] = [];
+    let list_two: number[] = [];
+    let totalSum: number = 0;
+    const lines = splitByLine(input);
+    lines.forEach(function (line: string): void {
+        list_one.push(Number(line.slice(0, 5)));
+        list_two.push(Number(line.slice(-5)));
+    });
+    list_one.forEach(function (num1: number): void {
+        let counter: number = 0;
+        list_two.forEach(function (num2: number): void {
+            if(num1 === num2) {
+                counter+=1;
+            }
+        })
+        totalSum += num1 * counter;
+    })
+    console.log(totalSum);
 }
 
 function splitByLine(input: string): string[] {
