73a8ea558aa4a0d5c4a303bc5ac4b8c8b6857972
gende0808 <gregiscool@live.nl>
2024-12-01 06:27:54 +0100

    one_a done

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,21 @@ one_a(input);
 
 
 function one_a(input : string): void {
-    console.log(splitByLine(input)[0]);
+    let list_one: number[] = [];
+    let list_two: number[] = [];
+    let totalSum: number = 0;
+    const lines = splitByLine(input);
+    lines.forEach(function (line: string): void {
+        list_one.push(Number(line.slice(0,5)));
+        list_two.push(Number(line.slice(-5)));
+    });
+    //sort arrays
+    list_one.sort();
+    list_two.sort();
+    for (let i = 0; i < list_one.length; i++) {
+        totalSum += Math.abs(list_one[i] - list_two[i]);
+    }
+    console.log(totalSum);
 }
 
 function one_b(): void {
